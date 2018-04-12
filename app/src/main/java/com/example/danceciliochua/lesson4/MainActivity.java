9092ecd0786157af25d2b45170a9079e628851d1
Dan Cecilio Chua <chua-d@cyscorpions.com>
2018-04-12 18:06:03 +0800

    Finish Lesson 4.1 Droid Cafe

--- app/src/main/java/com/example/danceciliochua/lesson4/MainActivity.java
+++ app/src/main/java/com/example/danceciliochua/lesson4/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.danceciliochua.lesson4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void showFoodOrder(String message) {
+        displayToast(message);
+    }
+
+    public void showDonutOrder(View view) {
+        showFoodOrder(getString(R.string.donut_order_message));
+    }
+
+    public void showIceCreamOrder(View view) {
+        showFoodOrder(getString(R.string.ice_cream_order_message));
+    }
+
+    public void showFroyoOrder(View view) {
+        showFoodOrder(getString(R.string.froyo_ordered_message));
     }
 }
