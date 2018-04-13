bb657beddfa29d015f37e0a86195f94941230b96
Dan Cecilio Chua <chua-d@cyscorpions.com>
2018-04-13 16:13:54 +0800

    Finish Lesson 4.2

--- app/src/main/java/com/example/danceciliochua/lesson4/MainActivity.java
+++ app/src/main/java/com/example/danceciliochua/lesson4/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.danceciliochua.lesson4;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
 
@@ -22,10 +28,15 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                displayMap();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     public void displayToast(String message) {
@@ -50,4 +61,47 @@ public class MainActivity extends AppCompatActivity {
     public void showFroyoOrder(View view) {
         showFoodOrder(getString(R.string.froyo_ordered_message));
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_order:
+                displayToast(getString(R.string.action_order_message));
+
+                return true;
+
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+
+                return true;
+
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+
+                return true;
+
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+
+                return true;
+
+            default:
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void displayMap() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+
+        String data = getString(R.string.google_mtv_coord_zoom12);
+        intent.setData(Uri.parse(data));
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
